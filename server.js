9fa746736c71e38610e32d0fed22d06e97a118cb
sumit4267 <ss59gms@gmail.com>
2025-06-20 15:14:21 +0530

    adding backend

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express"
+import "dotenv/config";
+import cors from "cors" ;
+import connectDB from "./configs/db.js";
+import { clerkMiddleware} from '@clerk/express'
+import ClerkWebhooks from "./controllers/clerkwebhooks.js";
+
+
+connectDB()
+const app = express()
+app.use(cors()) // Enable Cross-Origin Resource Sharing
+
+// middleware
+app.use(express.json())
+app.use(clerkMiddleware())
+
+
+// API to listen to Clerk Webhooks
+app.use("/api/clerk",ClerkWebhooks)
+
+
+
+app.get('/',(req, res) => res.send("API is working fine sumit"))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+
+
